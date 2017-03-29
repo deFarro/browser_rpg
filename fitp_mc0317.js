a096501ddb1d004e49e35a78b68555a89a083c7d
Nikita Svetlov <svetlov.n@gmail.com>
2017-03-29 19:27:44 +0300

    adding NPCs

--- fitp_mc0317.js
+++ fitp_mc0317.js
@@ -44,6 +44,7 @@ class Player extends Character{
     super(characterStats);
     this.int = characterStats.stats[2];
     this.luc = characterStats.stats[3];
+    this.companion = {name: '', hp: 0, attackRate: 0, defenceRate: 0};
   }
 //Метод для взлома физических замков, в зависимости от ловкости
   lockpick(target){
@@ -65,18 +66,23 @@ class Player extends Character{
   }
 //Метод для реализации механики взлома замка
   breakAnyLock(target, typeParam){
-    if (typeParam >= 5){
-      if (rand(0,9) < typeParam) {
-        target.lock.opened = true;
-        this.weapon = target.content;
-        console.log('Container opened');
+    if (!target.lock.opened){
+      if (typeParam >= 5){
+        if (rand(0,9) < typeParam) {
+          target.lock.opened = true;
+          this.weapon = target.content;
+          console.log('Container opened');
+        }
+        else {
+          console.log('Lockpick failed');
+        }
       }
       else {
-        console.log('Lockpick failed');
+        console.log('Lock is too complicated');
       }
     }
     else {
-      console.log('Lock is too complicated');
+      console.log('Container is empty');
     }
   }
 //Метод для восстановления здоровья, в зависимости от интеллекта. Без аргументов лечит себя
@@ -91,6 +97,28 @@ class Player extends Character{
     }
     console.log(`${this.name} healed ${target.name} by ${regenHp}`);
   }
+//Метод для взаимодействия с NPC: попросить присоединиться, вылечить или отдать оружие, с помощью интеллекта или силы
+  talk(target, ask = 'heal', forced = false){
+    let param = forced ? 'str' : 'int';
+    if (ask === 'heal'){
+      this.makeToDo(target, this[param], target[param], target.heal);
+    }
+    if (ask === 'assist'){
+      this.makeToDo(target, this[param], target[param], target.join);
+    }
+    if (ask === 'supply'){
+      this.makeToDo(target, this[param], target[param], target.supply);
+    }
+  }
+  //Метод убеждения NPC
+  makeToDo(target, typeParamPlayer, typeParamNPC, action){
+    if (typeParamPlayer > typeParamNPC) {
+      action.call(target, this);
+    }
+    else {
+      console.log(`${target.name} won't do it`);
+    }
+  }
 }
 
 //Класс противника. Использует объект со случайными данными для создания
@@ -101,6 +129,24 @@ class Enemy extends Character{
     this.level = protoStats.level;
   }
 }
+
+//Класс для NPC, с которыми игрок сможет взаимодействовать
+class NPC extends Enemy{
+  constructor(){
+    super();
+    this.name = npcNameGenerator();
+    this.int = Math.floor(this.level / 2);
+  }
+  join(target){
+    target.companion = {name: this.name, hp: this.hp, attackRate: this.weapon.demage + this.str, defenceRate: this.armor.defence + this.str};
+    console.log(`${this.name} joined ${target.name}`);
+  }
+  supply(target){
+    target.weapon = this.weapon;
+    console.log(`${this.name} passed weapon to ${target.name}`);
+  }
+}
+NPC.prototype.heal = Player.prototype.heal;
 
 class Container{
   constructor(){
@@ -133,7 +179,7 @@ class Armor{
 //Конструктор генерации объекта с данными для создания нового противника
 class NextEnemyStats {
   constructor(level){
-    this.name = nameGenerator();
+    this.name = enemyNameGenerator();
     this.stats = getEnemyStats();
     this.weapon = {demage: 2, apCost: 3};
     this.armor = {defence: 0};
@@ -177,7 +223,9 @@ function getEnemyStats(){
 }
 
 //Функция начала следующего хода - определяет что будет перед игроком - противник, контейнер или NPC
+var turn = 0;
 function startNextTurn(character){
+  console.log(`>>> Turn №${++turn}`);
   let index = rand(0, 5);
   switch (index){
     case 0:
@@ -187,12 +235,11 @@ function startNextTurn(character){
       break;
     case 3:
     case 4:
-    case 5:
       character.lockpick(new Container());
       character.hack(new Container());
       break;
-    // case 6:
-    //   character.talk(new NPC());
+    case 5:
+      character.talk(new NPC());
   }
 }
 
@@ -210,23 +257,32 @@ function fight(side1, side2){
   }
   console.log(`Fight is over`);
   battleDisplay(side1, side2);
+  if (!player1.dead){
+    player1.heal();
+  }
 }
 
 function rand(from, to){
   return Math.floor(Math.random() * (to - from + 1) + from);
 }
 
-function nameGenerator(){
+//Генератор имён роботов
+function enemyNameGenerator(){
   return 'RD-' + rand(1000, 9999);
 }
 
+//Генератор человеческих имён
+function npcNameGenerator(){
+  return names[0][rand(0, 4)] + ' ' + names[1][rand(0, 4)];
+}
+
 //-----------------------------
-var player = '{"name": "John Doe", "stats": [5, 5, 5, 5], "weapon": {"demage": 1, "apCost": 1}, "armor": {"defence": 0}}';
+var names = [['Jack', 'Nick', 'Mike', 'Jimmy', 'Frank'], ['Black', 'Brown', 'Green', 'White', 'Grey']];
+
+var player = '{"name": "John Doe", "stats": [10, 10, 10, 5], "weapon": {"demage": 1, "apCost": 1}, "armor": {"defence": 0}}';
 player = JSON.parse(player);
 var player1 = new Player(player);
 
 startNextTurn(player1);
-
-if (!player1.dead){
-  player1.heal();
-}
+startNextTurn(player1);
+startNextTurn(player1);
